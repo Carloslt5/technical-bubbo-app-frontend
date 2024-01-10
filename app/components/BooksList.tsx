db8097995fa00e276b1fd841466ff5f29b912dae
Carloslt <carloslt.proyectos@gmail.com>
2024-01-10 15:03:27 +0100

    add components

--- app/components/BooksList.tsx
+++ app/components/BooksList.tsx
@@ -2,21 +2,34 @@ import React from 'react'
 import { Text } from 'react-native'
 import { useSelector } from 'react-redux'
 import { type RootState } from './../store'
-import BookCard from './BookCard'
+// import BookCard from './BookCard'
+import { Link } from 'expo-router'
 
 const BooksList = () => {
   const { booksData } = useSelector((state: RootState) => state.booksData)
-
+  // const syles = StyleSheet.create({
+  //   padding: {
+  //     padding: 10,
+  //     margin: 10,
+  //     backgroundColor: 'grey',
+  //   },
+  // })
   return (
     <>
       {booksData === null ? (
         <Text>Loading...</Text>
       ) : (
         booksData.map((book, idx) => (
-          <BookCard
+          // <BookCard
+          //   key={idx}
+          //   {...book}
+          // />
+          <Link
+            href='/gallery/1234'
             key={idx}
-            {...book}
-          />
+          >
+            About
+          </Link>
         ))
       )}
     </>
